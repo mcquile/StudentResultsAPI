341a95d7d7a94573a68394eb907ee2f0a3129ea1
McQuile Karappen <mcquile@bbd.co.za>
2023-02-17 02:00:45 +0200

    Created modules controller

---
+++ Controllers/ModulesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentResultsAPI.CRUD;
+using StudentResultsAPI.Models.LecturerModels;
+using StudentResultsAPI.Models.ModulesModels;
+
+namespace StudentResultsAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ModuleController : Controller
+{
+    [HttpGet(Name = "GetAllModules")]
+    public IActionResult GetAllModules()
+    {
+        Module[] modules = ModuleCRUD.ReadAllModules();
+        return new ObjectResult(modules);
+    }
+
+    [HttpGet("{id}", Name = "GetModuleByID")]
+    public IActionResult GetModuleByID(int id)
+    {
+        Module module = ModuleCRUD.ReadModuleByID(id);
+        return new ObjectResult(module);
+    }
+
+    [HttpPost]
+    public IActionResult CreateNewModule([Bind("modulename")] ModuleWithoutID module)
+    {
+        if (module == null)
+        {
+            return BadRequest();
+        }
+
+        int moduleID = ModuleCRUD.CreateModule(module);
+
+        return CreatedAtRoute("GetModuleByID", new { id = moduleID }, module);
+    }
+
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateModule(
+        int id,
+        [Bind("modulename")] string moduleName = "")
+    {
+        ModuleWithoutID module = new ModuleWithoutID(moduleName);
+
+        int rowsAffected = ModuleCRUD.UpdateModule(id, module);
+
+        return new ObjectResult($"Rows affected: {rowsAffected}");
+    }
+
+    [HttpDelete("{id}", Name = "DeleteModuleByID")]
+    public IActionResult DeleteModuleByID(int id)
+    {
+        int rowsAffected = ModuleCRUD.DeleteModuleByID(id);
+        return new ObjectResult($"Number of rows affected: {rowsAffected}");
+    }
+}
