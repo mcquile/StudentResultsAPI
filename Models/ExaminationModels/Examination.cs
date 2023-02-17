58537586c75395aa9e045d891959f75abb1cbb9a
McQuile Karappen <mcquile@bbd.co.za>
2023-02-17 02:51:50 +0200

    Examinations models complete

---
+++ Models/ExaminationModels/Examination.cs
@@ -0,0 +1,49 @@
+﻿using Npgsql;
+
+namespace StudentResultsAPI.Models.ExaminationModels;
+
+/// <summary>
+/// Class that serves as a model for the Examinations table in the StudentResultsDB
+/// </summary>
+public class Examination
+{
+    public int id { get; set; }
+    public string title { get; set; }
+    public DateTime dateAndTime { get; set; }
+    public string buildingName { get; set; }
+
+    /// <summary>
+    /// Constructor to instantiate all attributes of class
+    /// </summary>
+    /// <param name="id">int</param>
+    /// <param name="title">string</param>
+    /// <param name="dateAndTime">DateTime</param>
+    /// <param name="buildingName">string</param>
+    public Examination(
+        int id,
+        string title,
+        DateTime dateAndTime,
+        string buildingName
+        )
+    {
+        this.id = id;
+        this.title = title;
+        this.dateAndTime = dateAndTime;
+        this.buildingName = buildingName;
+    }
+
+    /// <summary>
+    /// Maps the reader provide to a student object and returns the object.
+    /// </summary>
+    /// <param name="reader">NpgsqlDataReader</param>
+    /// <returns>Examination</returns>
+    public static Examination MapToStudent(NpgsqlDataReader reader)
+    {
+        int id = (int)reader["examinationid"];
+        string title = (string)reader["title"];
+        DateTime dateAndTime = (DateTime)reader["dateandtime"];
+        string buildingName = (string)reader["buildingname"];
+
+        return new Examination(id, title, dateAndTime, buildingName);
+    }
+}
---
+++ Models/ExaminationModels/ExaminationWithoutID.cs
@@ -0,0 +1,58 @@
+﻿using Npgsql;
+
+namespace StudentResultsAPI.Models.ExaminationModels;
+
+/// <summary>
+/// Class that serves as a model for the Examinations table in the StudentResultsDB
+/// </summary>
+public class ExaminationWithoutID
+{
+    public string title { get; set; }
+    public DateTime dateAndTime { get; set; }
+    public string buildingName { get; set; }
+
+    /// <summary>
+    /// Constructor to instantiate all attributes of class
+    /// </summary>
+    /// <param name="title">string</param>
+    /// <param name="dateAndTime">DateTime</param>
+    /// <param name="buildingName">string</param>
+    public ExaminationWithoutID(
+        string title,
+        DateTime? dateAndTime,
+        string buildingName
+    )
+    {
+        this.title = title;
+        this.dateAndTime = dateAndTime ?? DateTime.Now.Subtract(TimeSpan.FromDays(365));
+        this.buildingName = buildingName;
+    }
+
+
+    /// <summary>
+    /// Creates a dictionary entry for properties which are not null or empty.
+    /// </summary>
+    /// <returns>Dictionary</returns>
+    public Dictionary<string, string> mapDictionaryValues()
+        {
+            Dictionary<string, string> setDictionary = new Dictionary<string, string>();
+
+            if (this.title.Length != 0)
+            {
+                setDictionary.Add("title", this.title);
+            }
+
+            if (this.buildingName.Length != 0)
+            {
+                setDictionary.Add("buildingname", this.buildingName);
+            }
+
+            if (this.dateAndTime < DateTime.Now)
+            {
+                setDictionary.Add("dateandtime", this.dateAndTime.ToString());
+            }
+
+            return setDictionary;
+        }
+}
+
