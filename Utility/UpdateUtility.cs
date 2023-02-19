87f16e9f79b1c67d059c7dafbef71837f1d19ed5
McQuile Karappen <mcquile@bbd.co.za>
2023-02-19 11:34:47 +0200

    Commented on Update Query Builder Utility to make it easier to understand

--- Utility/UpdateUtility.cs
+++ Utility/UpdateUtility.cs
@@ -12,23 +12,29 @@ public class UpdateUtility
     /// <returns></returns>
     public static string generateUpdateQuery(string tableName, List<string> columnNames, KeyValuePair<string, string> idKeyValuePair)
     {
+        // If no columns require an update, return an empty string.
         if (columnNames.Count==0)
         {
             return "";
         }
+
+        // Declare stringbuilder and start it with "UPDATE *table name here* SET ".
         StringBuilder updateStringBuilder = new StringBuilder();
         updateStringBuilder.Append($"UPDATE {tableName.ToLower()} SET ");
 
+        // Loop through all columns provided and add it with a parameter and a comma. Example: name = @name, -> stringBuilder now holds: "UPDATE *table name here* SET name = @name, "
         foreach (string columnName in columnNames)
         {
             updateStringBuilder.Append($"{columnName} = @{columnName}, ");
         }
 
+        // If columns were added, the new stringbuilder will end with a comma and space. This statement removes the trailing comma and space.
         if (columnNames.Count>0)
         {
             removeTrailingCommaAndSpace(ref updateStringBuilder);
         }
-        
+
+        // Ends the stringBuilder with the WHERE statement holding the provided key-value pair. Example: The final string will be:  "UPDATE *table name here* SET name = @name WHERE *id name* = *id value*
         updateStringBuilder.Append($" WHERE {idKeyValuePair.Key} = {idKeyValuePair.Value}");
 
         return updateStringBuilder.ToString();
